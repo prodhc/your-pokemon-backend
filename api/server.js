c5abcf6069d917e9999f3b706064ef3244c56b47
prodhc <zionywood@gmail.com>
2021-06-08 20:33:02 -0700

    added to server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const cors = require('cors')
 const pokemonRouter = require('./pokemon/pokemon-router')
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 server.use('/api/pokemon', pokemonRouter)
+
+server.use('e', (req, res) => {
+    res.send(`<h1>Hello There</h1>`)
+})
 
 server.use('*', (req, res, next) => {
     next({ status: 404, message: "uhhhhh"})
@@ -15,7 +21,5 @@ server.use((err, req, res, next) => {
         message: err.message
     })
 })
-
-// DO YOUR MAGIC
 
 module.exports = server
